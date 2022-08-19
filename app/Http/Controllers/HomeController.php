d9acef1b31dd9ee01d213f4c368a7ca89c7491f8
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-08-18 22:23:06 -0300

    home introducao

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Repositories\ClientRepository;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __construct()
+    public function __construct(ClientRepository $clientRepository)
     {
-      
+      $this->clientRepository = $clientRepository;
     }
 
     public function index()
     {
+        $clients = Client::all();
         return view('home');
     }
 }
